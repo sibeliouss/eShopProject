511a6cf137034f6341c02810893ad2b50633fa3f
sibeliouss <sibelmozturk@hotmail.com>
2024-09-20 20:44:41 +0300

    .

--- eShop_CleanArch/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ eShop_CleanArch/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c96182e7ea3e5bb13d1e93a85c250818477fca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055cfe88f98d130a0dde11ad857a983a0c8d0daa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
